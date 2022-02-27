69ec2453e3307c36375203030a8949c63e2589fd
cankush625 <cankush625@gmail.com>
2022-02-27 22:45:40 +0530

    feat(Movie Song): Srivalli song from movie Pushpa
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Movie Songs/Pushpa/Srivalli/srivalli.rb
@@ -0,0 +1,299 @@
+use_synth :subpulse
+use_bpm 50
+
+# Repeat this starting music twice with change in last time (4th line of song)
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.32
+play (chord :Eb5, :major, sustain: 2)
+sleep 0.40
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 1)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.70
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.32
+play (chord :Eb5, :major, sustain: 2)
+sleep 0.40
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Db5, :major, sustain: 2)
+sleep 0.40
+play (chord :B5, :major, sustain: 1)
+sleep 0.25
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.70
+
+##| # Repeated -
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.32
+play (chord :Eb5, :major, sustain: 2)
+sleep 0.40
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 1)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.70
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :Gb5, :major)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.32
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.32
+play (chord :E5, :major, sustain: 2)
+sleep 0.32
+play (chord :Eb5, :major, sustain: 2)
+sleep 0.40
+
+play (chord :E5, :major)
+sleep 0.15
+play (chord :Ab5, :major)
+sleep 0.15
+play (chord :B5, :major)
+sleep 0.30
+play (chord :Db5, :major)
+sleep 0.25
+play (chord :A5, :major, sustain: 2)
+sleep 0.33
+play (chord :B5, :major)
+sleep 0.25
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.70
+
+
+use_synth :tri
+
+# NAZRE MILTE HI NAZRON SE
+
+use_bpm 40
+
+play (chord :E4, :major, sustain: 1)
+sleep 0.20
+play (chord :E4, :major)
+sleep 0.18
+play (chord :B4, :major, sustain: 1)
+sleep 0.27
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+
+# NAZRON KO CHURAYE
+
+use_bpm 50
+
+play (chord :B4, :major)
+sleep 0.18
+play (chord :B4, :major)
+sleep 0.18
+play (chord :A4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major)
+sleep 0.15
+play (chord :A4, :major)
+sleep 0.25
+play (chord :Ab4, :major, sustain: 1)
+sleep 0.45
+
+# KAISI YE HAYA TERI JO TU PALKO KO JHUKAYE
+
+use_bpm 40
+
+play (chord :E4, :major, sustain: 1)
+sleep 0.20
+play (chord :E4, :major)
+sleep 0.18
+play (chord :B4, :major, sustain: 1)
+sleep 0.27
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major, sustain: 1)
+sleep 0.25
+
+use_bpm 50
+
+play (chord :B4, :major)
+sleep 0.18
+play (chord :B4, :major)
+sleep 0.18
+play (chord :A4, :major, sustain: 1)
+sleep 0.25
+play (chord :B4, :major)
+sleep 0.15
+play (chord :B4, :major)
+sleep 0.15
+play (chord :A4, :major)
+sleep 0.25
+play (chord :Ab4, :major, sustain: 1)
+sleep 0.35
+
+# RAB JO POSHIDA HAI
+
+use_bpm 40
+
+play (chord :A4, :major)
+sleep 0.25
+play (chord :A4, :major, sustain: 2)
+sleep 0.40
+play (chord :A4, :major, sustain: 1)
+sleep 0.20
+play (chord :Ab4, :major)
+sleep 0.20
+play (chord :Gb4, :major)
+sleep 0.20
+play (chord :Ab4, :major, sustain: 1)
+sleep 0.12
+play (chord :Fb4, :major, sustain: 2)
+sleep 0.35
+
+# USKO NIHARE TU
+
+play (chord :A4, :major)
+sleep 0.25
+play (chord :A4, :major, sustain: 2)
+sleep 0.40
+play (chord :A4, :major, sustain: 1)
+sleep 0.20
+play (chord :Ab4, :major)
+sleep 0.20
+play (chord :Gb4, :major)
+sleep 0.20
+play (chord :Ab4, :major, sustain: 1)
+sleep 0.12
+play (chord :Fb4, :major, sustain: 2)
+sleep 0.35
+
+# AUR JO GARVIDA HAI USKO TALE TU
+
+play (chord :A4, :major)
+sleep 0.25
+play (chord :A4, :major, sustain: 2)
+sleep 0.40
+play (chord :A4, :major, sustain: 1)
+sleep 0.20
+play (chord :Ab4, :major)
+sleep 0.20
+play (chord :Gb4, :major)
+sleep 0.20
+play (chord :Ab4, :major, sustain: 1)
+sleep 0.12
+play (chord :E4, :major)
+sleep 0.30
+play (chord :E4, :major)
+sleep 0.20
+play (chord :Gb4, :major, sustain: 1)
+sleep 0.20
+play (chord :E4, :major)
+sleep 0.20
+play (chord :Eb4, :major, sustain: 1)
+sleep 0.20
+play (chord :E4, :major, sustain: 2)
+sleep 0.20
+play (chord :E5, :major, release: 1)
+sleep 0.40
+
+# TERI JHALAK ASHARFHI SRIVALLI
+
+use_bpm 40
+
+play (chord :E5, :major, sustain: 1)
+sleep 0.32
+play (chord :E5, :major)
+sleep 0.30
+play (chord :E5, :major, sustain: 1)
+sleep 0.20
+play (chord :E5, :major)
+sleep 0.20
+play (chord :E5, :major)
+sleep 0.20
+play (chord :E5, :major)
+sleep 0.20
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.15
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.35
+play (chord :Ab5, :major, sustain: 2)
+sleep 0.20
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.20
+play (chord :Ab5, :major)
+sleep 0.30
+play (chord :Gb5, :major, sustain: 2)
+sleep 0.25
+play (chord :E5, :major, sustain: 1)
+sleep 0.30
