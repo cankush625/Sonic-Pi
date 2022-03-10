4dbe35f9a78d91d8430f39b141782a326fa58953
cankush625 <cankush625@gmail.com>
2022-03-10 23:05:28 +0530

    feat(Faded song): Added new song lines
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- Alan Walker/faded.rb
+++ Alan Walker/faded.rb
@@ -83,7 +83,7 @@ sleep 0.35
 play (chord :Gb4, :major)
 sleep 0.35
 play (chord :F4, :major)
-sleep 0.65
+sleep 0.75
 play (chord :F4, :major)
 sleep 0.35
 play (chord :F4, :major)
@@ -91,4 +91,128 @@ sleep 0.35
 play (chord :Eb4, :major)
 sleep 0.35
 play (chord :Gb4, :major)
+sleep 1
+
+# AFRAID OUR AIM IS OUT OF SIGHT
+
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
 sleep 0.40
+play (chord :Eb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Eb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Ab4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.70
+
+# WANNA SEE US A-LIGHT
+
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.40
+play (chord :Db4, :major)
+sleep 0.40
+play (chord :Bb4, :major)
+sleep 1
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Ab4, :major)
+sleep 0.40
+play (chord :Gb4, :major)
+sleep 0.70
+
+# WHERE ARE YOU NOW?
+
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+sample :drum_splash_hard
+play (chord :Bb4, :major)
+sleep 3.6
+
+# WHERE ARE YOU NOW?
+
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+sample :drum_splash_soft
+play (chord :Bb4, :major)
+sleep 3.6
+
+# WHERE ARE YOU NOW?
+
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+sample :drum_splash_hard
+play (chord :Bb4, :major)
+sleep 0.80
+
+# WAS IT ALL IN MY FANTACY?
+
+play (chord :Gb4, :major)
+sleep 0.25
+play (chord :Ab4, :major)
+sleep 0.35
+play (chord :Ab4, :major)
+sleep 0.30
+play (chord :Ab4, :major)
+sleep 0.30
+play (chord :Bb4, :major)
+sleep 0.30
+play (chord :Ab4, :major)
+sleep 0.40
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.80
+
+# WHERE ARE YOU NOW?
+
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.35
+sample :drum_splash_hard
+play (chord :Bb4, :major)
+sleep 0.80
+
+# WERE YOU ONLY IMAGINARY?
+
+play (chord :Gb4, :major)
+sleep 0.30
+play (chord :Gb4, :major)
+sleep 0.30
+play (chord :Gb4, :major)
+sleep 0.30
+play (chord :Ab4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :F4, :major)
+sleep 0.35
+play (chord :F4, :major)
+sleep 0.35
+play (chord :F4, :major)
+sleep 0.80
