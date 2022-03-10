753919dd7091b385ff15d24aaba3fa9403917977
cankush625 <cankush625@gmail.com>
2022-03-10 23:04:22 +0530

    feat(Faded song): Faded song by Alan Walker
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Alan Walker/faded.rb
@@ -0,0 +1,94 @@
+use_synth :sine
+
+use_bpm 60
+
+# Starting Music
+
+2.times do
+  
+  play (chord :Gb4, :major)
+  sleep 0.75
+  play (chord :Gb4, :major)
+  sleep 0.75
+  play (chord :Gb4, :major)
+  sleep 0.75
+  play (chord :Bb4, :major)
+  sleep 0.80
+  
+  play (chord :Eb5, :major)
+  sleep 0.75
+  play (chord :Eb5, :major)
+  sleep 0.75
+  play (chord :Eb5, :major)
+  sleep 0.75
+  play (chord :Db5, :major)
+  sleep 0.80
+  
+  play (chord :Bb4, :major)
+  sleep 0.75
+  play (chord :Bb4, :major)
+  sleep 0.75
+  play (chord :Bb4, :major)
+  sleep 0.75
+  play (chord :Bb4, :major)
+  sleep 0.80
+  
+  play (chord :F4, :major)
+  sleep 0.75
+  play (chord :F4, :major)
+  sleep 0.75
+  play (chord :F4, :major)
+  sleep 0.75
+  play (chord :F4, :major)
+  sleep 0.80
+end
+
+sleep 0.20
+
+# YOU WERE THE SHADOW TO MY LIGHT
+
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.40
+play (chord :Eb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Eb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Ab4, :major)
+sleep 0.35
+play (chord :Bb4, :major)
+sleep 0.70
+
+# DID YOU FEEL US?
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Db4, :major)
+sleep 0.60
+play (chord :Bb4, :major)
+sleep 0.40
+
+# ANOTHER STAR YOU FADE AWAY
+
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.35
+play (chord :F4, :major)
+sleep 0.65
+play (chord :F4, :major)
+sleep 0.35
+play (chord :F4, :major)
+sleep 0.35
+play (chord :Eb4, :major)
+sleep 0.35
+play (chord :Gb4, :major)
+sleep 0.40
